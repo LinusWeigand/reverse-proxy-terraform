e6fd0dd3b64a1a683a653933132e156fa8f9a4fa
Linus Weigand <ge63nat@mytum.de>
2022-10-05 17:45:35 +0200

    variables

--- variables.tf
+++ variables.tf
@@ -1,1 +1,19 @@
+variable "domain" {
+  description = "The domain where the app will be hosted."
+  type        = string
+}
 
+variable "location" {
+  description = "Azure location to which resources should be deployed."
+  type        = string
+}
+
+variable "email" {
+  description = "Email address used when registering certificates with Let's Encrypt."
+  type        = string
+}
+
+variable "base_resource_name" {
+  description = "The base name to use for all resources."
+  type        = string
+}
